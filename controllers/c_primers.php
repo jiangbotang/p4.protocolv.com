b8d9c8067f809d64e494a5c76be1f764eb5e29c8
Jiangbo Tang <jiangbo.tang@gmail.com>
2013-12-22 17:25:09 -0500

    deployment test 2

--- controllers/c_primers.php
+++ controllers/c_primers.php
@@ -13,91 +13,90 @@ class primers_controller extends base_controller {
 	 * $itemPerPage : number of items to be displayed
 	 */
 	public function index($page = 1, $itemPerPage=5) {
-		Router::redirect("/users/login");
-		// // check user login status
-		// $this->loginCheck();
+		// check user login status
+		$this->loginCheck();
 
-		// # get item per page from method argument		
-		// $this->item_per_page = $itemPerPage;
-		// # get total number of pages
-		// $this->total_pages = $this->cal_total_pages();
+		# get item per page from method argument		
+		$this->item_per_page = $itemPerPage;
+		# get total number of pages
+		$this->total_pages = $this->cal_total_pages();
 
-		// // if requested page number is out of boundary, make page number 1
-		// if ($page < 1 || $page > $this->total_pages) {
-		// 	$page = 1;
-		// }
+		// if requested page number is out of boundary, make page number 1
+		if ($page < 1 || $page > $this->total_pages) {
+			$page = 1;
+		}
 
-		// // default item per page if out confinement (1-25)
-		// if ($itemPerPage > 0 && $itemPerPage < 26) {
-		// 	$this->item_per_page = $itemPerPage;
-		// } else {
-		// 	$this->item_per_page = 5;
-		// }
+		// default item per page if out confinement (1-25)
+		if ($itemPerPage > 0 && $itemPerPage < 26) {
+			$this->item_per_page = $itemPerPage;
+		} else {
+			$this->item_per_page = 5;
+		}
 
-		// # set the target page to current page
-		// $this->cur_page = $page;
+		# set the target page to current page
+		$this->cur_page = $page;
 
-		// # Setup up the view.
-		// # The View has a modular strucutre, e.g. template contains content, content
-		// # contains cat_con and primers_index_main...
-		// $this->template->content = View::instance('v_content');
-		// $this->template->content->cat_con = View::instance('v_cat_con');
-		// $this->template->content->cat_con->cat = View::instance('v_cat');
-		// $this->template->content->cat_con->con = View::instance('v_con');
-		// $this->template->content->primers_index_main = View::instance('v_primers_index_main');
-		// # set up view for navigate through pages of items
-		// $this->template->content->primers_index_main->item_nav = View::instance('v_item_nav');
+		# Setup up the view.
+		# The View has a modular strucutre, e.g. template contains content, content
+		# contains cat_con and primers_index_main...
+		$this->template->content = View::instance('v_content');
+		$this->template->content->cat_con = View::instance('v_cat_con');
+		$this->template->content->cat_con->cat = View::instance('v_cat');
+		$this->template->content->cat_con->con = View::instance('v_con');
+		$this->template->content->primers_index_main = View::instance('v_primers_index_main');
+		# set up view for navigate through pages of items
+		$this->template->content->primers_index_main->item_nav = View::instance('v_item_nav');
 
-		// # pass the new current page and total page number to the view
-		// $this->template->content->primers_index_main->item_nav->currentPage = $this->cur_page;
-		// $this->template->content->primers_index_main->item_nav->totalPages = $this->total_pages;
-		// $this->template->content->primers_index_main->item_nav->requestUrl = "/primers/index/";
+		# pass the new current page and total page number to the view
+		$this->template->content->primers_index_main->item_nav->currentPage = $this->cur_page;
+		$this->template->content->primers_index_main->item_nav->totalPages = $this->total_pages;
+		$this->template->content->primers_index_main->item_nav->requestUrl = "/primers/index/";
 
-		// $this->template->title = "primers";
-		// # set body tag id 'pageN'
-		// $this->template->pageN = "page5";
-		// # the archives menu as active menu
-		// $this->template->menu_arch = "menu_active";
+		$this->template->title = "primers";
+		# set body tag id 'pageN'
+		$this->template->pageN = "page5";
+		# the archives menu as active menu
+		$this->template->menu_arch = "menu_active";
 
-		// # Build the query
-		// $lower_b = ($page - 1) * $this->item_per_page + 1;
-		// $upper_b = $page * $this->item_per_page;
+		# Build the query
+		$lower_b = ($page - 1) * $this->item_per_page + 1;
+		$upper_b = $page * $this->item_per_page;
 
-		// # query for selected items
-		// $q = 'SELECT
-		// 		primers.primer_id,
-		// 		primers.name,
-		// 		primers.created,
-		// 		primers.modified,
-		// 		primers.user_id,
-		// 		primers.purpose,
-		// 		primers.restriction_enzyme,
-		// 		primers.sequence,
-		// 		primers.additional_info,
-		// 		users.first_name,
-		// 		users.last_name
-		// 	FROM primers
-		// 	INNER JOIN users
-		// 		ON primers.user_id = users.user_id
-		// 	WHERE primers.primer_id BETWEEN '."$lower_b".' AND '."$upper_b";
+		# query for selected items
+		$q = 'SELECT
+				primers.primer_id,
+				primers.name,
+				primers.created,
+				primers.modified,
+				primers.user_id,
+				primers.purpose,
+				primers.restriction_enzyme,
+				primers.sequence,
+				primers.additional_info,
+				users.first_name,
+				users.last_name
+			FROM primers
+			INNER JOIN users
+				ON primers.user_id = users.user_id
+			WHERE primers.primer_id BETWEEN '."$lower_b".' AND '."$upper_b";
 
-		// # Run the query
-		// $primers = DB::instance(DB_NAME)->select_rows($q);
+		# Run the query
+		$primers = DB::instance(DB_NAME)->select_rows($q);
 		
-		// # Pass the data to the View
-		// $this->template->content->primers_index_main->primers = $primers;
+		# Pass the data to the View
+		$this->template->content->primers_index_main->primers = $primers;
 
-		// # Create an array of 1 or many client files to be included in the head
-  //   	$client_files_body = Array(
-  //       '/js/item_nav.js'
-  //       );
+		# Create an array of 1 or many client files to be included in the head
+    	$client_files_body = Array(
+        '/js/item_nav.js'
+        );
 
-  //       # Use load_client_files to generate the links from the above array
-  //   	$this->template->client_files_body = Utils::load_client_files($client_files_body);
+        # Use load_client_files to generate the links from the above array
+    	$this->template->client_files_body = Utils::load_client_files($client_files_body);
 
 
-		// // echo $this->template->content->primers_index_main;
-		// echo $this->template;
+		// echo $this->template->content->primers_index_main;
+		echo $this->template;
 		
 	}
 
